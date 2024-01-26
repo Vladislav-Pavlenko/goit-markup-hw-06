633ac871e6fe7e6ef13a1175ab65aeaecbde0323
Vladislav-Pavlenko <151663067+Vladislav-Pavlenko@users.noreply.github.com>
2024-01-26 18:33:56 +0200

    hw6.5

--- sript.js
+++ sript.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   var openBtn = document.querySelector(".mob-open-btn");
   var closeBtn = document.querySelector(".mob-close-btn");
   var mobMenu = document.querySelector(".mob-menu");
+  var modWindow = document.querySelector(".backdrop");
+  var modCloseBtn = document.querySelector(".modal-close-button");
 
   openBtn.addEventListener("click", function () {
     mobMenu.classList.add("is-open");
@@ -9,5 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   closeBtn.addEventListener("click", function () {
     mobMenu.classList.remove("is-open");
+  });
+
+  modCloseBtn.addEventListener("click", function () {
+    modWindow.classList.remove("is-open");
   });
 });
